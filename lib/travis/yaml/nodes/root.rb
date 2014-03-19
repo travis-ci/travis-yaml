eecb82293e8c5103404cf9b0a32311e188a0fb6d
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-03-19 16:49:48 +0100

    add xcode_*

--- lib/travis/yaml/nodes/root.rb
+++ lib/travis/yaml/nodes/root.rb
@@ -2,26 +2,31 @@ module Travis::Yaml
   module Nodes
     class Root < Mapping
       LANGUAGE_SPECIFIC = {
-        bundler_args: %w[ruby],
-        compiler:     %w[c cpp],
-        lein:         %w[clojure],
-        otp_release:  %w[erlang],
-        gobuild_args: %w[go],
-        go:           %w[go],
-        jdk:          %w[clojure groovy java ruby scala],
-        ghc:          %w[haskell],
-        node_js:      %w[node_js],
-        ruby:         %w[ruby objective-c]
+        bundler_args:     %w[ruby],
+        compiler:         %w[c cpp],
+        lein:             %w[clojure],
+        otp_release:      %w[erlang],
+        gobuild_args:     %w[go],
+        go:               %w[go],
+        jdk:              %w[clojure groovy java ruby scala],
+        ghc:              %w[haskell],
+        node_js:          %w[node_js],
+        ruby:             %w[ruby objective-c],
+        xcode_sdk:        %w[objective-c],
+        xcode_scheme:     %w[objective-c],
+        xcode_project:    %w[objective-c],
+        xcode_workspace:  %w[objective-c],
+        xctool_args:      %w[objective-c],
       }
 
       map :language, required: true
       map :bundler_args, to: BundlerArgs
       map :deploy, :ruby, :os, :compiler, :git, :jdk
-      map :lein, :otp_release, :go, :ghc, :node_js, to: VersionList
+      map :lein, :otp_release, :go, :ghc, :node_js, :xcode_sdk, :xcode_scheme, to: VersionList
       map :rvm, to: :ruby
       map :otp, to: :otp_release
       map :node, to: :node_js
-      map :gobuild_args, to: Scalar[:str]
+      map :gobuild_args, :xcode_project, :xcode_workspace, :xctool_args, to: Scalar[:str]
       map :before_install, :install, :before_script, :script, :after_result, :after_script,
             :after_success, :after_failure, :before_deploy, :after_deploy, to: Stage
 
