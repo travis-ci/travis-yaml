ca1bc71fee68734674578528a1368eb036f79d8a
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-03-19 16:56:35 +0100

    add python option

--- lib/travis/yaml/nodes/root.rb
+++ lib/travis/yaml/nodes/root.rb
@@ -19,12 +19,13 @@ module Travis::Yaml
         xctool_args:      %w[objective-c],
         perl:             %w[perl],
         php:              %w[php],
+        python:           %w[python]
       }
 
       map :language, required: true
       map :bundler_args, to: BundlerArgs
       map :deploy, :ruby, :os, :compiler, :git, :jdk
-      map :lein, :otp_release, :go, :ghc, :node_js, :xcode_sdk, :xcode_scheme, :perl, :php, to: VersionList
+      map :lein, :otp_release, :go, :ghc, :node_js, :xcode_sdk, :xcode_scheme, :perl, :php, :python, to: VersionList
       map :rvm, to: :ruby
       map :otp, to: :otp_release
       map :node, to: :node_js
