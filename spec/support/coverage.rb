e63e3a6a0f82aa8b57945999195f4ffc7e14d8b7
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-03-29 17:31:39 +0800

    do not load simplecov on jruby

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -1,11 +1,11 @@
-require 'simplecov'
+unless defined? RUBY_ENGINE and RUBY_ENGINE == 'jruby'
+  require 'simplecov'
+  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 
-SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
-
-SimpleCov.start do
-  project_name 'travis-yaml'
-  coverage_dir '.coverage'
-
-  add_filter "/spec/"
-  add_group 'Library', 'lib'
+  SimpleCov.start do
+    project_name 'travis-yaml'
+    coverage_dir '.coverage'
+    add_filter '/spec/'
+    add_group 'Library', 'lib'
+  end
 end
