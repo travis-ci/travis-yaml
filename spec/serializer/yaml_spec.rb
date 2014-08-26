8878d2e308649f2885009ed5274870d5db713976
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-08-26 23:45:56 +0200

    get parens in order

--- spec/serializer/yaml_spec.rb
+++ spec/serializer/yaml_spec.rb
@@ -2,7 +2,7 @@ describe Travis::Yaml::Serializer::Yaml do
   subject(:config) { Travis::Yaml.parse('env: [{ secure: "foo" }, "bar"]') }
 
   example "serializes json" do
-    expect(config.serialize(:yaml)).gsub("'", "").to be ==
+    expect(config.serialize(:yaml).gsub("'", "")).to be ==
       "---\nenv:\n  matrix:\n  - !encrypted foo\n  - bar\nlanguage: ruby\nos:\n- linux\n"
   end
 
@@ -14,25 +14,25 @@ describe Travis::Yaml::Serializer::Yaml do
   example "serializes decrypted values" do
     config.decrypt { |*| "x" }
 
-    expect(config.serialize(:yaml, secure: :encrypted)).gsub("'", "").to be ==
+    expect(config.serialize(:yaml, secure: :encrypted).gsub("'", "")).to be ==
       "---\nenv:\n  matrix:\n  - !encrypted foo\n  - bar\nlanguage: ruby\nos:\n- linux\n"
 
-    expect(config.serialize(:yaml, secure: :decrypted)).gsub("'", "").to be ==
+    expect(config.serialize(:yaml, secure: :decrypted).gsub("'", "")).to be ==
       "---\nenv:\n  matrix:\n  - !decrypted x\n  - bar\nlanguage: ruby\nos:\n- linux\n"
   end
 
   example "avoid tags" do
     config.decrypt { |*| "x" }
 
-    expect(config.serialize(:yaml, secure: :encrypted, avoid_tags: true)).gsub("'", "").to be ==
+    expect(config.serialize(:yaml, secure: :encrypted, avoid_tags: true).gsub("'", "")).to be ==
       "---\nenv:\n  matrix:\n  - secure: foo\n  - bar\nlanguage: ruby\nos:\n- linux\n"
 
-    expect(config.serialize(:yaml, secure: :decrypted, avoid_tags: true)).to be ==
+    expect(config.serialize(:yaml, secure: :decrypted, avoid_tags: true).gsub("'", "")).to be ==
       "---\nenv:\n  matrix:\n  - x\n  - bar\nlanguage: ruby\nos:\n- linux\n"
   end
 
   example "indentation" do
-    expect(config.serialize(:yaml, indentation: 3)).gsub("'", "").to be ==
+    expect(config.serialize(:yaml, indentation: 3).gsub("'", "")).to be ==
       "---\nenv:\n   matrix:\n   - !encrypted foo\n   - bar\nlanguage: ruby\nos:\n- linux\n"
   end
 end
